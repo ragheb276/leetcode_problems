e5b55e4d2850e60b40126f705a5e600494a64ee7
ragheb276 <boody276@hotmail.com>
2025-07-20 07:14:54 +0300

    solution

--- Valid Anagram.py
+++ Valid Anagram.py
@@ -45,3 +45,12 @@ class Solution:
 
         return True
 #------------------------------------------------
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        c_s = Counter(s)
+        c_t = Counter(t)
+        if c_s == c_t:
+            return True
+        else:
+            return False
