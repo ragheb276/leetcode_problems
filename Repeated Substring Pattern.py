71bc4f4c2c01584aef8d54592515d6304d3f058f
ragheb276 <boody276@hotmail.com>
2025-07-21 06:37:51 +0300

    solution

---
+++ Repeated Substring Pattern.py
@@ -0,0 +1,14 @@
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        n = len(s)
+        for i in range(1, n // 2 + 1):
+            if n % i == 0 and s[:i] * (n // i) == s:
+                return True
+        
+        return False
+    
+    
+#------------------------------------------------
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        return s in (s+s)[1:-1]
